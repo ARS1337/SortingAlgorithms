07869f97383a0fad0fc82326d0ee67a19897b373
ARSl337 <abhaysingh17498@gmail.com>
2021-07-28 21:52:38 +0530

    serch algorithms

---
+++ src/com/company/Main.java
@@ -0,0 +1,45 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println(binaryRecursiveSearch(new int[]{0,1,2,3,4,5,6,7,8,11,45,100},0,11,45));
+        System.out.println(binaryIterativeSearch(new int[]{0,1,2,3,4,5,6,7,8,11,45,100},0,11,45));
+    }
+
+    public static int linearSearch(int[] a,int fintInt) {
+        int result = -1;
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] == fintInt) {
+                result = i;
+                break;
+            }
+        }
+        return result;
+    }
+
+    public static int binaryRecursiveSearch(int[] arr, int leftIndex, int rightIndex, int num){
+        if(leftIndex>rightIndex) return -1;
+        int mid = (leftIndex + rightIndex)/2;
+        System.out.println(leftIndex+" "+" "+mid+" "+rightIndex);
+        if(arr[mid] == num) return mid;
+        else if(arr[mid] > num ) return binaryRecursiveSearch(arr,leftIndex,mid-1,num);
+        else return binaryRecursiveSearch(arr,mid+1,rightIndex,num);
+    }
+
+    public static int binaryIterativeSearch(int[] arr, int leftIndex, int rightIndex, int num){
+        int result = -1;
+        while(leftIndex<=rightIndex){
+            int mid = (rightIndex+leftIndex)/2;
+            System.out.println(leftIndex+" "+" "+mid+" "+rightIndex);
+            if(arr[mid]==num){
+                result = mid;
+                break;
+            }
+            else if(arr[mid]<num) leftIndex = mid+1;
+            else rightIndex = mid - 1;
+        }
+        return result;
+    }
+}
