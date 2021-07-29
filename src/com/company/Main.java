f9083a0003e22bbb93f5673ebd148f926846a92c
ARSl337 <abhaysingh17498@gmail.com>
2021-07-29 20:24:11 +0530

    some sorting algo addedd

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,11 +3,107 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    int[] arr = new int[]{0,0,0,0,0,4,2,6,2,6,421,7,72,4,7,87634,23};
         //System.out.println(binaryRecursiveSearch(new int[]{0,1,2,3,4,5,6,7,8,11,45,100},0,11,45));
         //System.out.println(binaryIterativeSearch(new int[]{0,1,2,3,4,5,6,7,8,11,45,100},0,11,45));
-        //System.out.println(nLargestElement(new int[]{54,75,42,6,343,64,432,566,62362,5},10));
-        System.out.println(largestElement(new int[]{54,75,42,6,343,64,432,566,62362,5}));
+        //System.out.println(nLargestElement(arr,10));
+        //System.out.println(largestElement(arr));
+        //selectionSort(arr);
+        //insertionSort(arr);
+        //bubbleSort(arr);
+        //optimizedBubbleSort(arr);
+        //merge(arr,8,10,14);
+        mergeSort(arr,0,arr.length-1);
+
+        for(int i : arr) System.out.print(i+" ");
+    }
+
+    public static void mergeSort(int[] arr, int leftIndex, int rightIndex){
+        if(leftIndex<rightIndex){                               // exit condition : if both indexes are same
+            int mid = ( leftIndex + rightIndex )/2;             // keeps calling itself recursively until there is only 1 element in subSection of array
+            mergeSort(arr, leftIndex, mid);                     // i.e. leftIndex == rightIndex, after which it returns and merge is called subsequently
+            mergeSort(arr,mid+1, rightIndex);           // merge sorts already sorted subsections, and since only a single element can be considered sorted
+            merge(arr, leftIndex, mid, rightIndex);             // it just returns and compares 2 subsection with single element each
+        }                                                       // and just swaps them, now they act as sorted subsections with 2 element each
+    }                                                           // and merge can be used for sorting already sorted arrays
+
+    private static void merge(int[] arr, int leftIndex, int mid, int rightIndex){
+        int leftEnd = mid;                                                        // convert 2 sorted arrays into a single sorted array
+        int rightEnd = rightIndex;                                                // arr[] is sorted from leftIndex to mid
+        int i = leftIndex;                                                        // and also sorted from mid + 1 to right index
+        int j = mid + 1;                                                          // both these sorted sections are converted into a single sorted section
+        int[] temp = new int[rightIndex+1];                                       // from leftIndex to rightIndex
+        int tempIter = 0;
+        while(i <= leftEnd && j <= rightEnd){                                     // loop runs till either of the sorted arrays are exhausted
+            if(arr[i] < arr[j]){                                                  // compares elements and puts the smaller element in the temp array
+                temp[tempIter] = arr[i];
+                i++;                                                              // increment the index of the smaller element
+            }
+            else{
+                temp[tempIter] = arr[j];
+                j++;
+            }
+            tempIter++;                                                           // increment the index for the temp array
+        }
+
+        while (i<=leftEnd){                                                      // the unexhausted array is copied from its ith or jth current index till last index
+            temp[tempIter] = arr[i];
+            i++;
+            tempIter++;
+        }
+
+        while (j<=rightEnd) {
+            temp[tempIter] = arr[j];
+            j++;
+            tempIter++;
+        }
+        for(int k = 0, l=leftIndex; k <= (temp.length - 1) && l <= rightIndex; k++,l++) arr[l] = temp[k]; // copy elements from temp[] to arr[]
+    }
+
+    public static void bubbleSort(int[] arr){           // O(n^2)
+        for(int i = 0; i < arr.length; i++){            // loop runs from i = 0 till lastIndex
+            for(int j = 0; j < (arr.length-i-1); j++){  // loop runs from j = 0 till lastIndex-i for every i, as the largest element is shifted to the (lastIndex-i) position
+                if(arr[j]>arr[j+1]) {
+                    swap(arr, j, j + 1);             // compare with adjacent element, if greater, then swap
+                }
+            }
+            for(int k : arr) System.out.print(k+" ");
+            System.out.println();
+        }
+    }
+
+    public static void optimizedBubbleSort(int[] arr){           // O(n^2)
+        boolean isSorted ;
+        for(int i = 0; i < arr.length; i++){            // loop runs from i = 0 till lastIndex
+            isSorted = true;                            // array is assumed to be sorted on every iteration
+            for(int j = 0; j < (arr.length-i-1); j++){  // loop runs from j = 0 till lastIndex-i for every i, as the largest element is shifted to the (lastIndex-i) position
+                if(arr[j]>arr[j+1]) {
+                    swap(arr, j, j + 1);               // compare with adjacent element, if greater, then swap
+                    isSorted = false;                   // this is set to false only when elements arent in order i.e. unsorted
+                }
+            }
+            if(isSorted)  break;                        // if this was not set to false, then it means no preceding element was greater than
+            for(int k : arr) System.out.print(k+" ");   // its succeding element i.e. if(arr[j]>arr[j+1] wasnt triggered, break the loop
+            System.out.println();                       // as the array is sorted, no more iterations are needed
+        }
+    }
+
+    public static void insertionSort(int[] arr){           // O(n^2) time complexity
+        for(int i = 1; i < arr.length; i++){               // loop runs from i = 1 till i = lastIndex
+            for(int j = i; j > 0; j--){                    // loop runs from j = i till j = 1
+                if(arr[j-1]>arr[j]) swap(arr,j-1,j);    // compares (j-1)th element to the jth element and swaps if greater
+            }                                              // where j runs from i to 1. i.e. Keeps shifts smaller elements to leftmost
+//            for(int k : arr) System.out.print(k+" ");
+//            System.out.println();
+        }
+    }
+
+    public static void selectionSort(int[] arr){                 // O(n^2) time complexity
+        for(int i = 0; i<arr.length; i++){                       // outer loop runs from i = 0 till last index
+            for(int j = i; j< arr.length ; j++){                 // inner loop runs from j = i till last index
+                if(arr[i]>arr[j]) swap(arr,i,j);                 // arr[i] is compared to range(j = i,lastIndex) : arr[j] and smallest element
+            }                                                    // is shifted to the ith position
+        }
     }
 
     public static int linearSearch(int[] a,int fintInt) {
@@ -66,6 +162,16 @@ public class Main {
         return result;
     }
 
+    private static boolean isArraySorted(int[] arr){
+        boolean isSorted = true;
+        for(int i = 0; i< (arr.length-1); i++){
+            if(arr[i] > arr[i+1]){
+                isSorted = false;
+                break;
+            }
+        }
+        return isSorted;
+    }
     private static void swap(int[] arr, int i ,int j){
         int temp = arr[i];
         arr[i] = arr[j];
