1805c1052c56984f6f6c6f63f685f8e67a37463e
ARSl337 <abhaysingh17498@gmail.com>
2021-08-01 23:12:43 +0530

    clear

--- src/com/company/LeetCodeSolutions.java
+++ src/com/company/LeetCodeSolutions.java
@@ -57,39 +57,7 @@ public class LeetCodeSolutions {
         }
         return num >= 0 && true;
     }
-    public static ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        ListNode result = new ListNode();
-        ListNode temp = result;
 
-        if(l1.val < l2.val){
-            temp.val = l1.val;
-            l1 = l1.next;
-        }
-        else{
-            temp.val = l2.val;
-            l2 = l2.next;
-        }
-
-        while (l1 != null && l2 != null){
-            if(l1.val < l2.val){
-                temp.next = new ListNode(l1.val);
-                l1 = l1.next;
-            }else {
-                temp.next = new ListNode(l2.val);
-                l2 = l2.next;
-            }
-        }
-
-        while (l1 != null){
-            temp.next = new ListNode(l1.val);
-            l1 = l1.next;
-        }
-        while (l2 != null){
-            temp.next = new ListNode(l2.val);
-            l2 = l2.next;
-        }
-        return result;
-    }
 
     private static class ListNode {
         int val;
